a18ff6a59ac87df33f59a106027556213e51993f
Amit <amitreinich@gmail.com>
2020-12-06 16:41:53 +0200

    burger-menu

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,6 @@ gsap.from(".main-img", { opacity: 0, x: 50, duration: 1.2 });
 gsap.from(".project-card", {
   scrollTrigger: {
     trigger: ".project-card",
-    toggleActions: "play none play reset",
   },
   opacity: 0,
   y: -30,
@@ -39,7 +38,6 @@ gsap.from(".projects-h1", {
 gsap.from(".skills-icon", {
   scrollTrigger: {
     trigger: ".skills-icon",
-    toggleActions: "play reset play reset",
   },
   opacity: 0,
   y: -30,
@@ -52,7 +50,6 @@ gsap.from(".skills-icon", {
 gsap.from(".about-container", {
   scrollTrigger: {
     trigger: ".about-container",
-    toggleActions: "play reset play reset",
   },
   opacity: 0,
 
@@ -64,7 +61,6 @@ gsap.from(".about-container", {
 gsap.from(".about-img", {
   scrollTrigger: {
     trigger: ".about-container",
-    toggleActions: "play reset play reset",
   },
   opacity: 0,
   duration: 1.5,
@@ -76,7 +72,6 @@ gsap.from(".about-img", {
 gsap.from(".contact-container", {
   scrollTrigger: {
     trigger: ".contact-container",
-    toggleActions: "play reset play reset",
   },
   opacity: 0,
 
@@ -84,4 +79,51 @@ gsap.from(".contact-container", {
   ease: Power2.easeOut,
 });
 
+gsap.from(".contact-icon", {
+  scrollTrigger: {
+    trigger: ".contact-container",
+  },
+  opacity: 0,
+
+  duration: 1.5,
+  stagger: 0.1,
+  ease: Power2.easeOut,
+});
+
 // GSAP Animations end
+
+// Bubbles color animations
+const bubbles = document.querySelectorAll('[class^="bubbles"]');
+const soonCard = document.querySelector(".portfolio-card");
+const couponimCard = document.querySelector(".couponim-card");
+const prettierCard = document.querySelector(".prittier-card");
+
+soonCard.addEventListener("mouseover", () => changeBGColor("#57B268"));
+couponimCard.addEventListener("mouseover", () => changeBGColor("#4186A5"));
+prettierCard.addEventListener("mouseover", () => changeBGColor("#B64B5D"));
+
+function changeBGColor(color) {
+  bubbles.forEach((element) => {
+    element.style.backgroundColor = color;
+  });
+}
+
+// Burger menu
+const burgerMenuBtn = document.querySelector(".burger-menu");
+const sideMenu = document.querySelector(".side-menu");
+const body = document.querySelector("body");
+const burgerLinks = document.querySelectorAll(".burger-links li");
+
+//Menu open
+burgerMenuBtn.addEventListener("click", () => {
+  sideMenu.classList.toggle("open");
+  burgerMenuBtn.classList.toggle("toggle-btn");
+});
+
+// Each link clicked will reset the burger menu.
+burgerLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    sideMenu.classList.toggle("open");
+    burgerMenuBtn.classList.toggle("toggle-btn");
+  });
+});
