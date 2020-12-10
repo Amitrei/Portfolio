08385c99abf0d1ae14456abc2befcb1fede83ca4
Amit <amitreinich@gmail.com>
2020-12-10 23:06:17 +0200

    Modified mobile responsive

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,16 @@
 gsap.from(".main-headline-container", { opacity: 0, y: -50, duration: 1.2 });
 gsap.from(".main-img", { opacity: 0, x: 50, duration: 1.2 });
 
+// Main illustartion text decoration
+gsap.to(".main-illust-decoration", {
+  scrollTrigger: {
+    trigger: "main",
+    scrub: 0.5,
+  },
+  y: 100,
+  duration: 0.5,
+});
+
 gsap.from(".project-card", {
   scrollTrigger: {
     trigger: ".project-card",
@@ -35,9 +45,9 @@ gsap.from(".projects-h1", {
   ease: Power2.easeOut,
 });
 
-gsap.from(".skills-icon", {
+gsap.from(".skill-icon-wrapper", {
   scrollTrigger: {
-    trigger: ".skills-icon",
+    trigger: ".skill-icon-wrapper",
   },
   opacity: 0,
   y: -30,
@@ -90,14 +100,26 @@ gsap.from(".contact-icon", {
   ease: Power2.easeOut,
 });
 
+gsap.from(".hand-wave", {
+  scrollTrigger: {
+    trigger: ".contact-container",
+  },
+  opacity: 0,
+  rotation: 20,
+  duration: 1.5,
+  ease: Power2.easeOut,
+});
+
 // GSAP Animations end
 
-// Bubbles color animations
 const bubbles = document.querySelectorAll('[class^="bubbles"]');
 const soonCard = document.querySelector(".portfolio-card");
 const couponimCard = document.querySelector(".couponim-card");
 const prettierCard = document.querySelector(".prittier-card");
+const projectCover = document.querySelectorAll(".project-card");
+const projectContent = document.querySelectorAll(".project-content-container");
 
+// Bubbles color animations
 soonCard.addEventListener("mouseover", () => changeBGColor("#57B268"));
 couponimCard.addEventListener("mouseover", () => changeBGColor("#4186A5"));
 prettierCard.addEventListener("mouseover", () => changeBGColor("#B64B5D"));
